54a60239b01bb0d69ea9746d6a06d301bcce7ad3
matheuzildes <matheus17calazans@gmail.com>
2025-07-20 19:48:06 -0300

    5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,9 +5,6 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.bb614855f1bc4d98b047e40ec9720e37',
   appName: 'apptreino',
   webDir: 'dist',
-  server: {
-    url: 'https://bb614855-f1bc-4d98-b047-e40ec9720e37.lovableproject.com?forceHideBadge=true',
-    cleartext: true
   },
   plugins: {
     SplashScreen: {
